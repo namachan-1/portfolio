bc6b80178555dc2f5bf7f7cc33eab97e6658c83f
Nathaniel <nathaniel.chang828@gmail.com>
2025-09-04 11:36:41 -0700

    remove circle.svg to use a unicode character

--- src/app/components/sections/Hero/index.tsx
+++ src/app/components/sections/Hero/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import Image from 'next/image'
 import "./Hero.css"
 
 const Hero = () => {
   return (
-    <div className="section hero flex flex-col items-center mt-20 pt-20" id="hero">
-        <h1 className="hero-heading text-7xl md:text-8xl lg:text-9xl font-bold">Nathaniel
+    <div className="section hero flex flex-col items-center mt-[7rem] pt-[7rem]" id="hero">
+        <h1 className="hero-heading text-8xl md:text-[9rem] font-bold">Nathaniel
             <br /> Chang
-            <span className="red-dot relative inline-block"><Image src="./circle.svg" alt="Logo" width={50} height={50}/></span>
+            <span className="red-dot relative inline-block text-[--accent]">●</span>
         </h1>
 
-        <p className="hero-subheading flex justify-start text-md sm:text-lg md:text-xl mt-8 max-w-2xl font-geist-mono">
+        <p className="hero-subheading flex justify-start text-md sm:text-lg md:text-xl mt-8 max-w-md md:max-w-2xl font-geist-mono">
             Hi there! I&apos;m someone who loves to solves interesting problems, travel, and spend time with my wife and dog. Let&apos;s chat!
         </p>
         <h2></h2>
